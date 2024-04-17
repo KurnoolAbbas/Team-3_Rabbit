06b3e06d9c838f4b08cb5c37f98eb237d9f686d9
Chandana Musunuru <008449591@csusb.edu>
2024-04-17 14:08:33 -0700

    added google admob

---
+++ Assets/Scenes/Code/BannerScript.cs
@@ -0,0 +1,95 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using GoogleMobileAds;
+using GoogleMobileAds.Api;
+using System;
+
+public class BannerScript : MonoBehaviour
+{
+    // Sample ad unit ID for testing
+#if UNITY_ANDROID
+    private string _adUnitId = "ca-app-pub-3940256099942544/6300978111"; // Sample ad unit ID for testing on Android
+#elif UNITY_IPHONE
+    private string _adUnitId = "ca-app-pub-3940256099942544/2934735716"; // Sample ad unit ID for testing on iOS
+#else
+    private string _adUnitId = "unexpected_platform";
+#endif
+
+    BannerView _bannerView;
+
+    public void Start()
+    {
+        // Initialize the Google Mobile Ads SDK.
+        MobileAds.Initialize((InitializationStatus initStatus) =>
+        {
+            // This callback is called once the MobileAds SDK is initialized.
+            CreateBannerView();
+            EnableAds();
+        });
+    }
+
+    public void CreateBannerView()
+    {
+        Debug.Log("Creating banner view");
+
+        // If we already have a banner, destroy the old one.
+        if (_bannerView != null)
+        {
+            DestroyAd();
+        }
+
+        AdSize adSize = AdSize.Banner;
+        _bannerView = new BannerView(_adUnitId, adSize, 50, 700);
+    }
+
+    public void LoadAd()
+    {
+        // create an ad request
+        AdRequest adRequest = new AdRequest();
+
+        // Add keywords
+        adRequest.Keywords.Add("unity-admob-sample");
+
+        // send the request to load the ad.
+        Debug.Log("Loading banner ad.");
+        _bannerView.LoadAd(adRequest);
+        ListenToAdEvents();
+    }
+
+    public void EnableAds()
+    {
+        if (_bannerView == null)
+        {
+            CreateBannerView();
+            LoadAd();
+        }
+        else
+        {
+            _bannerView.Show();
+        }
+    }
+
+    public void DisableAds()
+    {
+        if (_bannerView != null)
+        {
+            DestroyAd();
+        }
+    }
+
+    private void ListenToAdEvents()
+    {
+        // Subscribe to banner ad events here.
+    }
+
+    public void DestroyAd()
+    {
+        if (_bannerView != null)
+        {
+            Debug.Log("Destroying banner ad.");
+            _bannerView.Destroy();
+            _bannerView = null;
+        }
+    }
+}
