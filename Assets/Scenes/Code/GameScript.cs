af7147a365b938275fa182a225f2bd61722f58c3
Chandana Musunuru <008449591@csusb.edu>
2024-05-16 09:45:52 -0700

    gamescript changes4

--- Assets/Scenes/Code/GameScript.cs
+++ Assets/Scenes/Code/GameScript.cs
@@ -3,36 +3,25 @@ using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
 
-
 public static class GameScript
 {
-    public static string baseAzureFunctionUrl = "https://team3-rabbitdev.azurewebsites.net/api/game/";
-    
-    public static string azureFunctionAuthenticationParams = "";
-
-
-
-
+    public static string baseAzureFunctionUrl = "https://team12.azurewebsites.net/api/game/";
 
     public static IEnumerator UpdateUserResponse(string gameID, bool validUserResponse, System.Action<bool> onSuccess, System.Action<string> onError)
     {
         string endpoint = $"{gameID}/update";
-        string queryParams = $"?{azureFunctionAuthenticationParams}&validUserResponse={validUserResponse}";
+        string queryParams = $"?validUserResponse={validUserResponse}";
         string fullUrl = baseAzureFunctionUrl + endpoint + queryParams;
 
-
         Debug.Log($"Updating user response as: {validUserResponse} at {fullUrl}");
-
 
         using (UnityWebRequest www = UnityWebRequest.Post(fullUrl, new WWWForm()))
         {
             yield return www.SendWebRequest();
 
-
             if (www.result != UnityWebRequest.Result.Success)
             {
                 string errorMessage = $"Failed to update user response: {www.error}";
-                //Debug.LogError(errorMessage);
                 onError?.Invoke(errorMessage);
             }
             else
@@ -43,25 +32,15 @@ public static class GameScript
         }
     }
 
-
-
-
-
-
-    
-
-
     public static IEnumerator GetUserHighestScore(string userId, System.Action<List<Game>> onSuccess, System.Action<string> onError)
     {
         Debug.Log("Calling Get User Score");
         string endpoint = $"getHighestGameStats";
-        string queryParams = $"?{azureFunctionAuthenticationParams}&userID={userId}";
+        string queryParams = $"?userID={userId}";
 
-        string url = $"{baseAzureFunctionUrl}{queryParams}";
-
+        string url = $"{baseAzureFunctionUrl}{endpoint}{queryParams}";
 
         Debug.Log($"Fetching game stats: {url}");
-
 
         using (UnityWebRequest www = UnityWebRequest.Get(url))
         {
@@ -70,8 +49,8 @@ public static class GameScript
             if (www.result != UnityWebRequest.Result.Success)
             {
                 string errorMessage = $"Failed to call API: {www.error}";
-                Debug.LogError(errorMessage);
-                onError?.Invoke(errorMessage);
+                Debug.Log("Failed to call API");
+               // onError?.Invoke(errorMessage);
             }
             else
             {
@@ -81,7 +60,6 @@ public static class GameScript
                 ApiResponse response = JsonUtility.FromJson<ApiResponse>(responseBody);
                 ApiResponses data = JsonUtility.FromJson<ApiResponses>(response.Content);
                 List<Game> gamesList = new List<Game>();
-
 
                 if (data != null && data.games != null)
                 {
@@ -103,8 +81,6 @@ public static class GameScript
                     Debug.LogWarning("Game object is null.");
                 }
 
-
-
                 onSuccess?.Invoke(gamesList);
             }
         }
@@ -113,12 +89,10 @@ public static class GameScript
     public static IEnumerator UpdateGameCompletedStats(string gameId, double accuracy, double completion, System.Action<bool> onSuccess, System.Action<string> onError)
     {
         string endpoint = $"{gameId}/complete";
-        string queryParams = $"?{azureFunctionAuthenticationParams}&accuracy={accuracy}&completion={completion}";
+        string queryParams = $"?accuracy={accuracy}&completion={completion}";
         string url = $"{baseAzureFunctionUrl}{endpoint}{queryParams}";
 
-
         Debug.Log($"Updating game completed stats: {url}");
-
 
         byte[] postData = System.Text.Encoding.UTF8.GetBytes("");
 
@@ -126,11 +100,9 @@ public static class GameScript
         {
             yield return www.SendWebRequest();
 
-
             if (www.result != UnityWebRequest.Result.Success)
             {
                 string errorMessage = $"Failed to call API: {www.error}";
-                //Debug.LogError(errorMessage);
                 onError?.Invoke(errorMessage);
             }
             else
@@ -141,25 +113,19 @@ public static class GameScript
         }
     }
 
-
-
-
     public static IEnumerator CreateNewGame(string userID, System.Action<string> onSuccess, System.Action<string> onError)
     {
         string endpoint = "create";
-        string queryParams = $"{azureFunctionAuthenticationParams}&userID={userID}";
-        string url = $"{baseAzureFunctionUrl}{endpoint}?{queryParams}";
+        string queryParams = $"?userID={userID}";
+        string url = $"{baseAzureFunctionUrl}{endpoint}{queryParams}";
         Debug.Log("Creating New Game" + url);
-
 
         List<IMultipartFormSection> formData = new List<IMultipartFormSection>();
         UnityWebRequest www = UnityWebRequest.Post(url, formData);
         yield return www.SendWebRequest();
 
-
         if (www.result != UnityWebRequest.Result.Success)
         {
-            // Debug.LogError("Failed to create game: " + www.error);
             Debug.Log("Failed to create game");
         }
         else
@@ -168,7 +134,6 @@ public static class GameScript
             Debug.Log("Received JSON data: " + responseBody);
             try
             {
-                // Assuming apiResponse is the JSON string received from the API
                 ApiResponse response = JsonUtility.FromJson<ApiResponse>(responseBody);
                 Database data = JsonUtility.FromJson<Database>(response.Content);
                 string gameID = data.gameId;
@@ -177,7 +142,6 @@ public static class GameScript
             catch (System.Exception ex)
             {
                 string errorMessage = "Error parsing JSON data: " + ex.Message;
-                //Debug.LogError(errorMessage);
                 onError?.Invoke(errorMessage);
             }
         }
